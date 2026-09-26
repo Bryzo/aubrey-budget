dd6572ad2714db0f852151c49ab7ab6f44e3f610
Aubrey Mmotong <mmotong.aa@gmail.com>
2026-09-26 09:54:26 +0200

    Update service-worker.js
    updated move funds function

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,60 @@
-const CACHE_NAME = "aubrey-budget-v1";
+const CACHE_NAME = "aubrey-budget-v2";
 
 const FILES_TO_CACHE = [
     "./",
-    "./index.html",
-    "./manifest.json"
+    "./manifest.json",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
 ];
 
+// Install new service worker
 self.addEventListener("install", event => {
-
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(FILES_TO_CACHE);
-            })
+            .then(cache => cache.addAll(FILES_TO_CACHE))
     );
 
+    // Activate the new version immediately
+    self.skipWaiting();
 });
 
-self.addEventListener("fetch", event => {
-
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-
-                return response || fetch(event.request);
-
-            })
+// Remove old caches
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
     );
 
+    self.clients.claim();
+});
+
+// Network first, cache fallback
+self.addEventListener("fetch", event => {
+
+    if (event.request.method !== "GET") {
+        return;
+    }
+
+    event.respondWith(
+        fetch(event.request)
+            .then(response => {
+
+                const copy = response.clone();
+
+                caches.open(CACHE_NAME)
+                    .then(cache => {
+                        cache.put(event.request, copy);
+                    });
+
+                return response;
+            })
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
 });
